390adae3cd3473648d9d8e6e275d02b49ec182f9
Endriyas <endriyas@uni.minerva.edu>
2024-06-17 02:12:40 +0300

    adding_btns

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
+from cell import Cell
 import settings
+import utils
 
 root = Tk()
 
@@ -34,6 +36,16 @@ center_frame.place(
     x=utils.width_prct(25),
     y=utils.height_prct(25),
 )
+
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell()
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column=x, row=y
+        )
+
+
 
 # Run the window
 root.mainloop()
