e67b18a5af1c9eb4f4ec3e524ec4dba5226c6a38
Endriyas <endriyas@uni.minerva.edu>
2024-06-19 00:02:06 +0300

    fixing errors

--- cell.py
+++ cell.py
@@ -11,3 +11,17 @@ class Cell:
             text = 'Text'
         )
         self.cell_btn_object = btn
+    def create_btn_object(self, location):
+        btn = Button(
+            location,
+            text="Text",
+        )
+        btn.bind('<Button-1', self.left_click_actions)
+        btn.bind('<Button-3', self.right_click_actions)
+
+    def left_click_actions(self, event):
+        print(event)
+        print('left clicked')
+    def right_click_actions(self, event):
+        print(event)
+        print('right clicked')
